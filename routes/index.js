9b56bf30965ce4b98062a334672504887bbd5214
Dawid Potocki <dawid@dawidpotocki.com>
2020-08-07 13:49:38 +1200

    Add login/register pages

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,14 @@ router.get('/welcome', function(req, res, next) {
   res.render('welcome');
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+
 router.get('/app', function(req, res, next) {
   res.render('app');
 });
